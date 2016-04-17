dfaa77e37e29d597ea79abf29cad2f99cbd0eedc
Sam Gruse <sgruse89@gmail.com>
2016-04-17 12:47:08 -0700

    protractor testing for DOM minipulation is complete

--- test/people-spec.js
+++ test/people-spec.js
@@ -1,24 +1,56 @@
-// describe('Protractor Testing', function () {
-//   it('should have a title', function () {
-//     browser.get('http://localhost:8080');
-//
-//     expect(browser.getTitle()).toEqual('Client Side!!!');
-//   });
-// });
-//
-
-
-// spec.js
-describe('Protractor Demo App', function() {
-  it('should add a person', function() {
+describe('Protractor Testing', function () {
+  it('should have a title', function () {
     browser.get('http://localhost:8080');
-    element(by.id('create-button')).click();
-    element(by.id('name-field')).sendKeys('sam');
-    // element(by.model('person.age')).sendKeys(2);
 
-    element(by.id('save-button')).click();
-    console.log("TESTING");
-    expect(element(by.id('result-name')).getText()).
-        toEqual('sam'); // This is wrong!
+    expect(browser.getTitle()).toEqual('Client Side!!!');
   });
+});
+
+//END TO END TESTING WITH PROTRACTOR:
+describe('Testing input fields for new person creation', function() {
+
+//BEFORE EACH BLOCK TO START BROWSER:
+beforeEach(function() {
+  browser.get('http://localhost:8080');
+});
+
+// TESTING FOR THE CREATION OF A NEW PERSON
+  it('should add a person with name and age', function() {
+    element(by.css('button.lime-button')).click();
+    element(by.model('person.name')).sendKeys('sam');
+    element(by.model('person.age')).sendKeys(26);
+
+    element(by.css('button.save-button')).click();
+    expect(element(by.binding('person.name')).getText()).
+      toEqual('Name: sam');
+    expect(element(by.binding('person.age')).getText()).
+      toEqual('Age: 26');
+  });
+
+// TESTING FOR THE UPDATE OF AN EXISTING PERSON
+  it('Should update the first record with a new name and age', function() {
+    element(by.css('button.show-button')).click();
+    element(by.css('button.green-button')).click();
+    element(by.css('input.update-name')).clear();
+    element(by.css('input.update-name')).sendKeys('Jordan');
+    element(by.css('input.update-age')).clear();
+    element(by.css('input.update-age')).sendKeys(100);
+
+    element(by.css('button.save-update')).click();
+    element(by.css('button.show-button')).click();
+    expect(element(by.binding('person.name')).getText()).
+      toEqual('Name: Jordan');
+    expect(element(by.binding('person.age')).getText()).
+      toEqual('Age: 100');
+
+  });
+
+// TESTING DELETION OF A PERSON
+  it('Should delete the updated user', function() {
+    element(by.css('button.show-button')).click();
+    element(by.css('button.red-button')).click();
+    element(by.css('button.show-button')).click();
+    // EXPECT THE SECTION BLOCK TO BE EMPTY!!!
+
+  })
 });
