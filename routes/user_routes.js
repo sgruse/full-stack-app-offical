89ae3aa27015f5eda394ab4937d0b92e59615fea
Sam Gruse <sgruse89@gmail.com>
2016-06-20 23:35:45 -0700

    Fixing issue with user route

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -4,8 +4,6 @@ const jsonParser = require('body-parser').json();
 const User = require(__dirname + '/../models/user');
 const handleDBError = require(__dirname + '/../lib/handle_db_error');
 
-// const router = module.exports = exports = express.Router();
-
 module.exports = (apiRouter) => {
 
 apiRouter.route('/currentuser')
